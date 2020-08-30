5501807a627a607aa36f5a8ee22a6048a6e58755
Daivik Bijoy <iamdaivik@gmail.com>
2020-08-30 06:22:56 +0530

    The whole project is done. Only need validation from dad

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const va_misc = document.querySelector("[va-misc]");
 const va_result = document.querySelector(".va-result");
 const va_form = document.querySelector(".va-form");
 const clear = document.querySelector(".clear");
+let sum;
 
+// backup elements
+const bu_form = document.querySelector(".bu-form");
+const noOfBattery = document.querySelectorAll("input[type='radio']");
+const batteryAh = document.querySelector("[battery-ah]");
+const batteryResult = document.querySelector(".result-2");
+const clear2 = document.querySelector(".clear-2");
 let va;
 // Event Listener on VA calculate
 
@@ -25,19 +32,22 @@ va_form.addEventListener("submit", function (e) {
   ) {
     // do nothing
   } else {
-    const sum =
+    sum =
       va_tube.value * 20 +
       va_bulb.value * 9 +
       va_fan.value * 75 +
       va_tv.value * 100 +
       va_lap.value * 50 +
-      va_desk.value * 100;
-    let va = 1.2 * ((sum + 20) / 0.8);
+      va_desk.value * 100 +
+      20;
+    va = 1.2 * (sum / 0.8);
 
     // Displaying it on the monitor
 
     va_result.value = `${va.toFixed(2)} VA`;
     va_result.style.display = "block";
+    console.log(va);
+    console.log(sum);
   }
 });
 
@@ -52,9 +62,42 @@ totalInputs.forEach(function (current) {
 });
 
 // To clear every input field of VA
-clear.addEventListener("click", function () {
+clear.addEventListener("click", function (e) {
   totalInputs.forEach(function (current) {
     current.value = "";
     va_result.style.display = "none";
   });
+  noOfBattery.forEach(function (current) {
+    if (current.checked) {
+      current.checked = false;
+    }
+  });
+  batteryAh.value = "";
+  e.preventDefault();
+});
+
+// For backup time
+bu_form.addEventListener("submit", function (e) {
+  let batteryValue;
+  noOfBattery.forEach(function (current) {
+    if (current.checked) {
+      batteryValue = current.value;
+      console.log(batteryValue);
+    }
+  });
+  let backupTime = (batteryAh.value * batteryValue * 9.6) / (sum * va);
+  batteryResult.value = backupTime;
+  batteryResult.style.display = "block";
+  console.log(va);
+  console.log(sum);
+  e.preventDefault();
+});
+
+clear2.addEventListener("click", function () {
+  noOfBattery.forEach(function (current) {
+    if (current.checked) {
+      current.checked = false;
+    }
+  });
+  batteryAh.value = "";
 });
